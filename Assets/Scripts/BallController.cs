c74c5a281acf7090d73b1afef84c273ed5d448d3
AliaksandraValodzina <shoora.volodina@gmail.com>
2019-01-17 23:43:15 +0300

    4, 5 lessons

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    bool isStarted;
+    Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isStarted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isStarted)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rigidbody.velocity = new Vector3(speed, 0, 0);
+                isStarted = true;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SwitchDirection();
+        }
+    }
+
+    void SwitchDirection()
+    {
+        if(rigidbody.velocity.z > 0)
+        {
+            rigidbody.velocity = new Vector3(speed, 0, 0);
+        } else if (rigidbody.velocity.x > 0)
+        {
+            rigidbody.velocity = new Vector3(0, 0, speed);
+        }
+    }
+}
