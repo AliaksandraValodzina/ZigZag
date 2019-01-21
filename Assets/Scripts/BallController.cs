e5753dbbcec89d2b127e20aa755585d18a2ac566
AliaksandraValodzina <shoora.volodina@gmail.com>
2019-01-22 01:09:05 +0300

    test

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,6 +7,7 @@ public class BallController : MonoBehaviour
     [SerializeField]
     private float speed;
     bool isStarted;
+    bool gameOver;
     Rigidbody rigidbody;
 
     private void Awake()
@@ -18,6 +19,7 @@ public class BallController : MonoBehaviour
     void Start()
     {
         isStarted = false;
+        gameOver = false;
     }
 
     // Update is called once per frame
@@ -32,7 +34,17 @@ public class BallController : MonoBehaviour
             }
         }
 
-        if (Input.GetMouseButtonDown(0))
+        Debug.DrawRay(transform.position, Vector3.down, Color.red);
+
+        if (!Physics.Raycast(transform.position, Vector3.down, 1f))
+        {
+            gameOver = true;
+            rigidbody.velocity = new Vector3(0, -25f, 0);
+
+            Camera.main.GetComponent<CameraFollow>().gameOver = true;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !gameOver)
         {
             SwitchDirection();
         }
