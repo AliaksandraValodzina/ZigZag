c75d04f4293a09a460f668ce36b4b34f7f1f31ee
AliaksandraValodzina <shoora.volodina@gmail.com>
2019-01-27 15:40:14 +0300

    27 01 19

--- Assets/Scripts/UIMAnager.cs
+++ Assets/Scripts/UIMAnager.cs
@@ -1,13 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIMAnager : MonoBehaviour
 {
+    public static UIMAnager instance;
+    public GameObject zigZagPanel;
+    public GameObject gameOverPanel;
+    public GameObject tabText;
+    public Text score;
+    public Text highScoreOne;
+    public Text highScoreTwo;
+
+    private void Awake()
+    {
+        if (instance = null)
+        {
+            instance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void GameStart()
+    {
+        tabText.SetActive(false);
+        zigZagPanel.GetComponent<Animator>().Play("panelUp");
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+
+    }
+
+    public void ResetFunction()
+    {
+        SceneManager.LoadScene(0);
     }
 
     // Update is called once per frame
